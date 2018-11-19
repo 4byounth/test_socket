dfef933519398aeda50b14b6f32e4f68493deab2
我的mac <4byounth@4byounthdemac.local>
2018-11-19 20:00:54 +0800

    添加程序开始语句

--- test_socket/AppDelegate.m
+++ test_socket/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"程序开始");
     return YES;
 }
 
